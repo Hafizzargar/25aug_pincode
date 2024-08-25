7c804deabb383d2d69ea09a8667c45a7aec339f0
Hafizzargar <hafezzargar987@gmail.com>
2024-08-25 09:07:48 +0530

    new

---
+++ src/compenent/high.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import './high.css'
+
+function High(props) {
+    const {val,setval}=props;
+    const [dis,setdis]=useState(false);
+    
+    function abc(e){
+        if(e.target.value.length>0 || e.target.value.length<=5 || e.target.value.length>6){
+            setdis(true);
+
+        }
+        setval(e.target.value);
+      
+      }
+  return (
+    <div>
+      <label>Enter Pincode</label>
+      <input
+      type="text"
+        className="main_input"
+        placeholder="Pincode"
+        value={val}
+        onChange={abc}
+      />
+       <Link to="/data">
+        <button>Lookup</button>
+      </Link>
+    
+     
+      
+    </div>
+  );
+}
+
+export default High;
